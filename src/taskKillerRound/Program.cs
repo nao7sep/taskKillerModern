778476b2ef10e7aef690eb11b785fb597b830596
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2025-12-13 11:23:58 +0900

    Add project files for taskKillerCore, taskKillerRound, and taskKillerTests

---
+++ src/taskKillerRound/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskKiller.Round
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
